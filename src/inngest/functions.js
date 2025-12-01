aacf1370dbd524e257e29fdbb7d50297c4b022fb
ManrajSingSra <srasinghmanraj@gmail.com>
2025-12-01 13:23:29 +0530

    max2origin

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -1,15 +1,26 @@
 import { inngest } from "./client";
+import { gemini,createAgent } from "@inngest/agent-kit";
+
 
 
 export const hello=inngest.createFunction(
     {name:"hello"},
     {event:"hello/h"},
     async({event,step})=>{
+        const data=event.data.data.value;
 
-        const data=event.data.data.value
+        const codeAgent=createAgent({
+            name:"Summarizer",
+            system:"You are next.js developer. You write readble, maintainable code. You write the componets like button from scratch.",
+            model:gemini({model:"gemini-2.0-flash"})
+        })
 
-        console.log("Ok");
-        return {data}
+        const {output}=await codeAgent.run(`Write the code snippet of ${data}`);
+        console.log(output[0].content);
+        
+    
+
+        return {data:output[0].content}
     }
     
 );
